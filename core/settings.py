bdb8a20de253c393324f047a4ac22f5e176e2fbf
Ndaru <daruprasetiyo123@gmail.com>
2025-06-23 13:39:09 +0700

    Mengubah SECRET_KEY

--- core/settings.py
+++ core/settings.py
@@ -1,15 +1,13 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6y&of!m5!f!egzj8wh#hi*(#uu_+%_g*zjhn#%jpjml_4)jg8('
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -45,7 +43,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -60,9 +58,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
-# ==== Database ====
-
 # SQLite Database
 DATABASES = {
     'default': {
@@ -71,20 +66,7 @@ DATABASES = {
     }
 }
 
-# PostgreSQL Databse
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': '<dbname>',                               # Your Database Name
-#         'USER': '<postgres>',                             # Your databse user
-#         'PASSWORD': '<password>',                         # Your database password
-#         'HOST': '<localhost>',                            # Your database host
-#         'PORT': '<5432>',                                 # Your database port
-#     }
-# }
-# =======================
 
-# ==== Password validation ====
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -116,7 +98,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static",
 )
 
 # Default primary key field type
